654d58717e9368d32660b39da5c262e5ad8ff2b5
name here <email>
2020-08-01 02:43:46 -0700

    Coming soon

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ class Home extends React.Component {
 
     render() {
         return(
-            <Title level={1} style={{textAlign: 'center'}}>Coming Soon!</Title>
+            <Title level={5} style={{textAlign: 'center'}}>Coming Soon!</Title>
         );
     }
 }
