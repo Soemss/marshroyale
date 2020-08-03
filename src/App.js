e97a5836b3b9f3fb0b550fa7e113f376d541b005
name here <email>
2020-08-03 03:11:55 -0700

    404

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import NotFound from './components/NotFound';
+import { createMuiTheme, ThemeProvider, withStyles } from '@material-ui/core/styles';
 
 class App extends React.Component {
     constructor() {
@@ -13,16 +14,16 @@ class App extends React.Component {
     render() {
         return(
             <Router>
-                <Navbar />
-                <Switch>
-                    <Redirect from='/' to='/home' exact />
-                    <Route
-                        path='/home'
-                        exact
-                        component={Home}
-                    />
-                    <Route component={NotFound} />
-                </Switch>
+                    <Navbar />
+                    <Switch>
+                        <Redirect from='/' to='/home' exact />
+                        <Route
+                            path='/home'
+                            exact
+                            component={Home}
+                        />
+                        <Route component={NotFound} />
+                    </Switch>
             </Router>
         )
     }
