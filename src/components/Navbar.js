b2e7ce0a00be0ebd0b59375ec790e355234974c0
name here <email>
2020-08-06 00:26:32 -0700

    Punc Errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppBar, Tabs, Tab, Typography, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link, useLocation } from 'react-router-dom';
-import { useTheme } from '@material-ui/core/styles'
+import { useTheme } from '@material-ui/core/styles';
 
 
 const useStyles = makeStyles((theme) => ({
