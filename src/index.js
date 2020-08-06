52755ce15809d99b36c1a972ac75549fd4db15ce
name here <email>
2020-08-06 00:09:08 -0700

    MUI Theme

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core'
+import { grey } from 'material-ui/core/colors/grey'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +13,23 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: grey,
+    secondary: {
+      main: grey[800]
+      // light:
+      // dark:
+    }
+  }
+})
+
+render(
+  <MuiThemeProvider theme = {theme}>
+      <App />
+  </MuiThemeProvider>
+)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
